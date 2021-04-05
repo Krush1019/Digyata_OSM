0336bdff771514990f54d019ebd7e2d881fbc53d
meet847 <75733889+meet847@users.noreply.github.com>
2021-04-05 12:33:13 +0530

    RASHMIN change to contact page.

--- bootstrap/cache/config.php
+++ bootstrap/cache/config.php
@@ -197,7 +197,7 @@
       'file' => 
       array (
         'driver' => 'file',
-        'path' => 'E:\\backup php\\Digyata\\storage\\framework/cache/data',
+        'path' => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\storage\\framework/cache/data',
       ),
       'memcached' => 
       array (
@@ -464,12 +464,12 @@
       'local' => 
       array (
         'driver' => 'local',
-        'root' => 'E:\\backup php\\Digyata\\storage\\app',
+        'root' => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\storage\\app',
       ),
       'public' => 
       array (
         'driver' => 'local',
-        'root' => 'E:\\backup php\\Digyata\\storage\\app/public',
+        'root' => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\storage\\app/public',
         'url' => 'http://localhost/storage',
         'visibility' => 'public',
       ),
@@ -515,13 +515,13 @@
       'single' => 
       array (
         'driver' => 'single',
-        'path' => 'E:\\backup php\\Digyata\\storage\\logs/laravel.log',
+        'path' => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\storage\\logs/laravel.log',
         'level' => 'debug',
       ),
       'daily' => 
       array (
         'driver' => 'daily',
-        'path' => 'E:\\backup php\\Digyata\\storage\\logs/laravel.log',
+        'path' => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\storage\\logs/laravel.log',
         'level' => 'debug',
         'days' => 14,
       ),
@@ -585,7 +585,7 @@
       'theme' => 'default',
       'paths' => 
       array (
-        0 => 'E:\\backup php\\Digyata\\resources\\views/vendor/mail',
+        0 => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\resources\\views/vendor/mail',
       ),
     ),
     'log_channel' => NULL,
@@ -726,7 +726,7 @@
     'lifetime' => '120',
     'expire_on_close' => false,
     'encrypt' => false,
-    'files' => 'E:\\backup php\\Digyata\\storage\\framework/sessions',
+    'files' => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\storage\\framework/sessions',
     'connection' => NULL,
     'table' => 'sessions',
     'store' => NULL,
@@ -746,9 +746,9 @@
   array (
     'paths' => 
     array (
-      0 => 'E:\\backup php\\Digyata\\resources\\views',
+      0 => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\resources\\views',
     ),
-    'compiled' => 'E:\\backup php\\Digyata\\storage\\framework\\views',
+    'compiled' => 'B:\\Documents\\WebProjects\\clgProject\\Git\\Digyata\\storage\\framework\\views',
   ),
   'debug-server' => 
   array (
