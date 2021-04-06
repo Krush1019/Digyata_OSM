cf7aa9a35f61dd4b11004d41e4defa6e628d3486
parth-html <76615401+parth-html@users.noreply.github.com>
2021-04-06 23:30:31 +0530

    parth client detail update

--- app/Http/Controllers/client_user/user/ClientDetailController.php
+++ app/Http/Controllers/client_user/user/ClientDetailController.php
@@ -17,15 +17,17 @@ class ClientDetailController extends Controller
     } catch (DecryptException $e) {
       return view('/pages/error-404');
     }
-    
+
     $services = DB::table('tbl_ser_list')
                 ->where('ser_id', '=', $decrypted)
                 ->join('tbl_service_catalogs as tsc', 'tbl_ser_list.ser_cat_id', '=', 'tsc.id')
-                ->join('tbl_client_manage as tcm', 'tbl_ser_list.ser_cat_id', '=', 'tcm.id')
+                // ->join('tbl_client_manage as tcm', 'tbl_ser_list.ser_cat_id', '=', 'tcm.id')
                 ->first();
+                $ser_id = array_map('intval', explode(',', $services->ser_item_id));
+                array_pop($ser_id);
 
     $items = DB::table('tbl_ser_item_price')
-                ->where('ser_id', '=', $decrypted)
+                ->whereIn('item_id', $ser_id)
                 ->get();
       return view('/pages/client_user/user/client-detail')->with('service',$services)->with('items',$items);
     }
