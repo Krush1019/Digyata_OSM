b27fdbf01925ba341dd6fa4d6045f37300c2a697
Krush1019 <krushangprakashbhai1910@gmail.com>
2021-04-09 22:02:02 +0530

    KRUSHANG Client-listing page update

--- resources/views/pages/client_user/user/confirm-msg.blade.php
+++ resources/views/pages/client_user/user/confirm-msg.blade.php
@@ -33,10 +33,10 @@
                                                 <div class="icon icon--order-success svg add_bottom_15">
                                                       <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72">
                                                             <g fill="none" stroke="#8EC343" stroke-width="2">
-                                                                  <circle cx="36" cy="36" r="35"
+                                                                  <circle cx="36" cy="36" r="35" 
                                                                         style="stroke-dasharray:240px, 240px; stroke-dashoffset: 480px;">
                                                                   </circle>
-                                                                  <path d="M17.417,37.778l9.93,9.909l25.444-25.393"
+                                                                  <path d="M17.417,37.778l9.93,9.909l25.444-25.393" 
                                                                         style="stroke-dasharray:50px, 50px; stroke-dashoffset: 0px;">
                                                                   </path>
                                                             </g>
@@ -52,7 +52,6 @@
                                                 </div>
                                           </div>
                                     </div>
-
                               </div>
                         </div>
                         <!-- /box_booking -->
