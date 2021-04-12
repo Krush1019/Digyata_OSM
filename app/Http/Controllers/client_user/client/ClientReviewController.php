95e893996b71266dca629e9204cfdc39912cf565
parth-html <76615401+parth-html@users.noreply.github.com>
2021-04-12 23:34:58 +0530

    parth changes client side reviews

--- app/Http/Controllers/client_user/client/ClientReviewController.php
+++ app/Http/Controllers/client_user/client/ClientReviewController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers\client_user\client;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class ClientReviewController extends Controller
-{   
+{
   public function __construct() {
     $this->middleware("auth:client");
 }
 
     public function index(){
+      $reviews = DB::table('tbl_review_orders')
+                 ->join('tbl_user_manage as tum', 'tbl_review_orders.uID', '=', 'tum.id')
+                 ->get();
+                //  dd($reviews);
       $breadcrumbs = [['link' => "/client-dashboard", 'name' => "Dashboard"], ['name' => "Reviews"]];
       return view('/pages/client_user/client/client-review', [
         'breadcrumbs' => $breadcrumbs
-      ]);
+      ])->with('reviews',$reviews);
     }
 }
