8023166768ccff48f1b116e53ee7f8c12bc29c63
parth-html <76615401+parth-html@users.noreply.github.com>
2021-04-22 21:51:50 +0530

    parth updated changes

--- routes/web.php
+++ routes/web.php
@@ -134,7 +134,7 @@ use App\Http\Controllers\LanguageController;
     /** confirm -- order */
     Route::get('/confirm-order/{id}', 'client_user\user\ConfirmOrderController@index')->name('confirm-order');
     Route::get('/order/confirm/{id}', function ($id) {
-      return view('pages\client_user\user\confirm-msg',['orderId'=>decrypt($id)]);
+      return view('pages/client_user/user/confirm-msg',['orderId'=>decrypt($id)]);
     })->name('confirm.msg');
 
     /** Order -- Invoice */
